b9e0a4465fe669ae800b6ce724ab16b9102b9c78
RJ <rj.rosopa.iii@gmail.com>
2020-06-17 20:42:21 +0800

    slideshow mobile fix

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+(function(){
+    if(document.querySelectorAll('.slide')){
+        function goToSlide(slideNumber){
+            var slides = document.querySelectorAll('.slide');
+            
+            if(slideNumber >= slides.length || slideNumber < 0){
+                slideNumber = 0;
+            }
+            
+            slides.forEach(x => x.classList.remove('active'));
+            slides[slideNumber].classList.add('active');
+        }
+
+        function getCurrentSlideNumber(){
+            var slides = document.querySelectorAll('.slide');
+            for(var i = 0; i < slides.length; i++){
+                if(slides[i].classList.contains('active')){
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        setInterval(function(){
+
+            goToSlide(getCurrentSlideNumber() + 1);
+
+        }, 4000);
+
+        var prev = document.querySelector('.slideshow > .prev');
+        if(prev){
+            prev.onclick = function(){
+                goToSlide(getCurrentSlideNumber() - 1);
+            }
+        }
+
+        var next = document.querySelector('.slideshow > .next');
+        if(next){
+            next.onclick = function(){
+                goToSlide(getCurrentSlideNumber() + 1);
+            }
+        }
+    }
+})();
